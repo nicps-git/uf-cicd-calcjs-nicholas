ee87fb4041bff910e54aa315225d459adb0893d7
nicps-git <nicps_@hotmail.com>
2025-09-06 14:50:31 -0300

    Adicionanda função de potencia.

--- src/calculadora.js
+++ src/calculadora.js
@@ -13,9 +13,15 @@
         return a * b;
     }
 
+    // Função de potenciação
+    function power(base, exponent) {
+        return Math.pow(base, exponent);
+    }
+
     // Exportar as funções
     module.exports = {
       add,
       subtract,
-      multiply
+      multiply,
+      power
     };
