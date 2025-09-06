614a904868d027ee83de570c3440ef233a52b9d2
nicps-git <nicps_@hotmail.com>
2025-09-06 14:45:29 -0300

    Revert "Adicionado função de subtração e exportação das funções"
    This reverts commit ebe3f9941f509a3d5e699a29489d412f0d39eeb7.

--- src/calculadora.js
+++ src/calculadora.js
@@ -8,22 +8,8 @@
         return a - b;
     }
 
-    // Função de multiplicação
-    function multiply(a, b) {
-        return a * b;
-    }
-
-    // Função de divisão
-    function divide(a, b) {
-        if (b === 0) {
-        throw new Error("Divisão por zero não é permitida.");
-        }
-        return a / b;
-    }
-
     // Exportar as funções
     module.exports = {
-      add,
-      subtract,
-      divide,
-    };
+        add,
+        subtract
+    }; 
